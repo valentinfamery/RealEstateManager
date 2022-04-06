d866c7d19240c2315a4353277418dcf73ed0fe17
Azubal <valentinfamery087@gmail.com>
2022-04-06 16:05:45 +0200

    fix(MainActivity.class) : Replace correct id resource for textViewMain because the previous resource referred to the second activity Add String.valueOf() method to correctly display the int-type quantity variable

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
@@ -23,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void configureTextViewMain(){
         this.textViewMain.setTextSize(15);
-        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+        this.textViewMain.setText(R.string.textViewMainBase);
     }
 
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(String.valueOf(quantity));
     }
 }
