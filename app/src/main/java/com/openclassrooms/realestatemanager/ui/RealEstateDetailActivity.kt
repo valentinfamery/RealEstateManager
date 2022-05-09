90fb072ea9ef9e6935301ec4a2d50e5a2b3c8e45
Azubal <valentinfamery087@gmail.com>
2022-05-09 21:20:08 +0200

    feat(MainActivity.kt): add new button for edit in RealEstateDetailActivity.kt

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/RealEstateDetailActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/RealEstateDetailActivity.kt
@@ -12,10 +12,12 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,98 +45,129 @@ class RealEstateDetail : ComponentActivity() {
 @Composable
 fun Greeting2() {
 
-    ConstraintLayout() {
+    Scaffold(
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { /* do something */ },
+                icon = { Icon(Icons.Filled.Edit, "Localized description") },
+                text = { Text(text = "Edit") },
+                modifier = Modifier.clip(RoundedCornerShape(15.dp))
+            )
+        },
+        content = {
 
-        val (centerAlignedTopAppBar,lazyVerticalGrid) = createRefs()
+            ConstraintLayout() {
 
-        CenterAlignedTopAppBar(
-            title = {
-                Text(text = "Estate Manager")
-            },
-            navigationIcon = {
-                val activity = (LocalContext.current as? Activity)
-                IconButton(onClick = {
-                    activity?.finish()
-                }) {
-                    Icon(Icons.Filled.ArrowBack,"")
-                }
-            },
-            modifier = Modifier.constrainAs(centerAlignedTopAppBar) {
-                top.linkTo(parent.top, margin = 0.dp)
-                start.linkTo(parent.start, margin = 0.dp)
-                end.linkTo(parent.end, margin = 0.dp)
-            }
+                val (centerAlignedTopAppBar, lazyVerticalGrid) = createRefs()
 
-        )
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Estate Manager")
+                    },
+                    navigationIcon = {
+                        val activity = (LocalContext.current as? Activity)
+                        IconButton(onClick = {
+                            activity?.finish()
+                        }) {
+                            Icon(Icons.Filled.ArrowBack, "")
+                        }
+                    },
+                    modifier = Modifier.constrainAs(centerAlignedTopAppBar) {
+                        top.linkTo(parent.top, margin = 0.dp)
+                        start.linkTo(parent.start, margin = 0.dp)
+                        end.linkTo(parent.end, margin = 0.dp)
+                    }
 
-        LazyVerticalGrid(
-            modifier = Modifier.constrainAs(lazyVerticalGrid) {
-                top.linkTo(centerAlignedTopAppBar.bottom, margin = 10.dp)
-            },
-            columns = GridCells.Adaptive(minSize = 150.dp)
-        ) {
-            item {
-                Card(
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp, vertical = 8.dp)
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(corner = CornerSize(16.dp))
+                )
+
+                LazyVerticalGrid(
+                    modifier = Modifier.constrainAs(lazyVerticalGrid) {
+                        top.linkTo(centerAlignedTopAppBar.bottom, margin = 10.dp)
+                    },
+                    columns = GridCells.Adaptive(minSize = 150.dp)
                 ) {
-                    Row {
-                        Column(
+                    item {
+                        Card(
                             modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth()
-                                .align(Alignment.CenterVertically)
+                                .padding(horizontal = 8.dp, vertical = 8.dp)
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(corner = CornerSize(16.dp))
                         ) {
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineLarge)
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineSmall)
+                            Row {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth()
+                                        .align(Alignment.CenterVertically)
+                                ) {
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineLarge
+                                    )
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineSmall
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    item {
+                        Card(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 8.dp)
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(corner = CornerSize(16.dp))
+                        ) {
+                            Row {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth()
+                                        .align(Alignment.CenterVertically)
+                                ) {
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineLarge
+                                    )
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineSmall
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    item {
+                        Card(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 8.dp)
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(corner = CornerSize(16.dp))
+                        ) {
+                            Row {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .fillMaxWidth()
+                                        .align(Alignment.CenterVertically)
+                                ) {
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineLarge
+                                    )
+                                    Text(
+                                        text = "VIEW DETAIL",
+                                        style = MaterialTheme.typography.headlineSmall
+                                    )
+                                }
+                            }
                         }
                     }
                 }
             }
-            item {
-                Card(
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp, vertical = 8.dp)
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(corner = CornerSize(16.dp))
-                ) {
-                    Row {
-                        Column(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth()
-                                .align(Alignment.CenterVertically)
-                        ) {
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineLarge)
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineSmall)
-                        }
-                    }
-                }
-            }
-            item {
-                Card(
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp, vertical = 8.dp)
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(corner = CornerSize(16.dp))
-                ) {
-                    Row {
-                        Column(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth()
-                                .align(Alignment.CenterVertically)
-                        ) {
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineLarge)
-                            Text(text = "VIEW DETAIL", style = MaterialTheme.typography.headlineSmall)
-                        }
-                    }
-                }
-            }
-        }
-    }
+
+        })
 
 
 
