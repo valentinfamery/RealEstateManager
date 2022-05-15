965b71c1d2be8ea228399c9de611bce51b4699a6
Azubal <valentinfamery087@gmail.com>
2022-05-15 23:48:48 +0200

    feat(MapScreen and dependency): Add Google Map Library and Map in MapScreen.kt

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/MapScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/MapScreen.kt
@@ -2,10 +2,7 @@ package com.openclassrooms.realestatemanager.ui.screens
 
 import android.content.Intent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,6 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import com.openclassrooms.realestatemanager.ui.RealEstateDetail
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -28,7 +31,7 @@ import kotlinx.coroutines.launch
 fun MapScreen(drawerState: DrawerState, scope: CoroutineScope) {
     ConstraintLayout() {
 
-        val (centerAlignedTopAppBar) = createRefs()
+        val (centerAlignedTopAppBar,map) = createRefs()
 
         CenterAlignedTopAppBar(
             title = {
@@ -52,6 +55,29 @@ fun MapScreen(drawerState: DrawerState, scope: CoroutineScope) {
                 end.linkTo(parent.end, margin = 0.dp)
             }
         )
+
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+
+
+        GoogleMap(
+            modifier = Modifier.fillMaxSize().constrainAs(map){
+                top.linkTo(centerAlignedTopAppBar.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+
+
     }
 
 }
