7c7ce19228b758a20688c1f2889509b152d1c211
Azubal <valentinfamery087@gmail.com>
2022-07-09 21:27:09 +0200

    feat : all ckeckbox in detail view realestate for point interested

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/RealEstateDetailScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/RealEstateDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.openclassrooms.realestatemanager.ui.screens
 
 import android.app.Activity
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,21 +9,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.google.accompanist.flowlayout.FlowColumn
-import com.google.accompanist.flowlayout.FlowRow
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
@@ -32,11 +29,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.openclassrooms.realestatemanager.R
-import com.openclassrooms.realestatemanager.models.RealEstate
-import com.openclassrooms.realestatemanager.ui.ui.theme.Projet_9_OC_RealEstateManagerTheme
 import com.openclassrooms.realestatemanager.viewmodels.RealEstateViewModel
 import com.skydoves.landscapist.glide.GlideImage
-import java.io.Serializable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,7 +56,9 @@ fun RealEstateDetailScreen(realEstateViewModel: RealEstateViewModel, itemId: Str
                 .verticalScroll(rememberScrollState())
                 .fillMaxHeight()) {
 
-                val (centerAlignedTopAppBar,textType,textPrice,textArea,textNumberRoom,textDescription,textAddress,textPointsOfInterest,textStatus,textDateOfEntry,textDateOfSale,textRealEstateAgent,lazyColumnPhoto,googleMap) = createRefs()
+                val (centerAlignedTopAppBar,textType,textPrice,textArea,textNumberRoom,textDescription,textAddress,textStatus,textDateOfEntry,textDateOfSale,textRealEstateAgent,lazyColumnPhoto,googleMap) = createRefs()
+
+                val (rowHopital,rowSchool,rowShops,rowParks) = createRefs()
 
                 CenterAlignedTopAppBar(
                     title = {
@@ -90,16 +86,33 @@ fun RealEstateDetailScreen(realEstateViewModel: RealEstateViewModel, itemId: Str
                     end.linkTo(parent.end, margin = 25.dp)
                 }) {
                     repeat(listPhotos?.size ?: 0) {
-                        Box(modifier = Modifier
-                            .size(184.dp)
-                            .clip(RoundedCornerShape(15.dp))) {
+                        Box(modifier = Modifier.size(184.dp)) {
 
                             Column() {
+                                ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+                                    val (image, text) = createRefs()
+
                                     GlideImage(
                                         imageModel = listPhotos?.get(it)?.photoUrl,
                                         contentScale = ContentScale.Crop,
+                                        modifier = Modifier.constrainAs(image) {
+                                            top.linkTo(parent.top, margin = 0.dp)
+                                            start.linkTo(parent.start, margin = 0.dp)
+                                            end.linkTo(parent.end, margin = 0.dp)
+                                        }
                                     )
-                                    Text(text = listPhotos?.get(it)?.text ?:"" )
+                                    Text(
+                                        text = listPhotos?.get(it)?.text ?:"" ,
+                                        modifier = Modifier.constrainAs(text) {
+                                            top.linkTo(image.bottom, margin = 0.dp)
+                                            start.linkTo(parent.start, margin = 0.dp)
+                                            end.linkTo(parent.end, margin = 0.dp)
+                                        })
+
+                                }
+
+
                             }
                         }
                     }
@@ -158,8 +171,87 @@ fun RealEstateDetailScreen(realEstateViewModel: RealEstateViewModel, itemId: Str
                     Text(text = itemRealEstate?.numberAndStreet.toString())
                 }
 
+                Row(
+                    modifier = Modifier
+                        .constrainAs(rowHopital) {
+                            top.linkTo(textAddress.bottom, margin = 5.dp)
+                            start.linkTo(parent.start, margin = 50.dp)
+                            end.linkTo(parent.end, margin = 50.dp)
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+
+                    ) {
+
+                    itemRealEstate?.hopitalNear?.let { it1 ->
+                        Checkbox(
+                            checked = it1,
+                            onCheckedChange = { itemRealEstate?.hopitalNear = it },
+                        )
+                    }
+                    Text(text = "Near Hopital")
+                }
+
+                Row(
+                    modifier = Modifier
+                        .constrainAs(rowSchool) {
+                            top.linkTo(rowHopital.bottom, margin = 5.dp)
+                            start.linkTo(parent.start, margin = 50.dp)
+                            end.linkTo(parent.end, margin = 50.dp)
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+
+                    ) {
+                    itemRealEstate?.SchoolNear?.let { it1 ->
+                        Checkbox(
+                            checked = it1,
+                            onCheckedChange = { itemRealEstate?.SchoolNear = it }
+                        )
+                    }
+                    Text(text = "Near School")
+                }
+
+                Row(
+                    modifier = Modifier
+                        .constrainAs(rowShops) {
+                            top.linkTo(rowSchool.bottom, margin = 5.dp)
+                            start.linkTo(parent.start, margin = 50.dp)
+                            end.linkTo(parent.end, margin = 50.dp)
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                ) {
+                    itemRealEstate?.shopsNear?.let { it1 ->
+                        Checkbox(
+                            checked = it1,
+                            onCheckedChange = { itemRealEstate?.shopsNear = it }
+                        )
+                    }
+                    Text(text = "Near Shops")
+                }
+
+                Row(
+                    modifier = Modifier
+                        .constrainAs(rowParks) {
+                            top.linkTo(rowShops.bottom, margin = 5.dp)
+                            start.linkTo(parent.start, margin = 50.dp)
+                            end.linkTo(parent.end, margin = 50.dp)
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                ) {
+                    itemRealEstate?.parksNear?.let { it1 ->
+                        Checkbox(
+                            checked = it1,
+                            onCheckedChange = { itemRealEstate?.parksNear = it }
+                        )
+                    }
+                    Text(text = "Near Parks")
+                }
+
                 Row(modifier = Modifier.constrainAs(textStatus) {
-                    top.linkTo(textAddress.bottom, margin = 25.dp)
+                    top.linkTo(rowParks.bottom, margin = 25.dp)
                     start.linkTo(parent.start, margin = 0.dp)
                     end.linkTo(parent.end, margin = 0.dp)
                 }) {
