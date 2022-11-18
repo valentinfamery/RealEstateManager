a115f7cc7f27df4dd1e50e115d6b08acb22dcd63
azubal <valentinfamery087@gmail.com>
2022-11-18 14:31:23 +0100

    fix(): 10/10 filters okay

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/ListScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/ListScreen.kt
@@ -27,10 +27,13 @@ import com.openclassrooms.realestatemanager.ui.FilterActivity
 import com.openclassrooms.realestatemanager.utils.WindowSize
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
 import java.util.*
 
-@SuppressLint("UnrememberedMutableState")
+
+@SuppressLint("UnrememberedMutableState", "SimpleDateFormat")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(
@@ -44,43 +47,62 @@ fun ListScreen(
 ) {
     val context = LocalContext.current
     val realEstates by realEstateViewModel.realEstates.collectAsState()
-    var filterState by remember {mutableStateOf(false)}
-    var realEstatesFilter by remember { mutableStateOf(listOf<RealEstateDatabase>())}
+    var filterState by remember { mutableStateOf(false) }
+    var realEstatesFilter by remember { mutableStateOf(listOf<RealEstateDatabase>()) }
 
-    val launcherActivityResult = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {activityResult->
-        if(activityResult.resultCode == 0){
-           filterState = activityResult.data?.getBooleanExtra("filterState",false)!!
-        }
-        if(activityResult.resultCode == 1){
-            filterState = activityResult.data?.getBooleanExtra("filterState",true)!!
-            val type = activityResult.data?.getStringExtra("type")!!
-            val city = activityResult.data?.getStringExtra("city")!!
-            val minSurface = activityResult.data?.getIntExtra("minSurface",0)!!
-            val maxSurface = activityResult.data?.getIntExtra("maxSurface",0)!!
-            val minPrice = activityResult.data?.getIntExtra("minPrice",0)!!
-            val maxPrice = activityResult.data?.getIntExtra("maxPrice",0)!!
-            val onTheMarketLessALastWeek = activityResult.data?.getBooleanExtra("onTheMarketLessALastWeek",false)!!
-            val soldOn3LastMonth     = activityResult.data?.getBooleanExtra("soldOn3LastMonth",false)!!
-            val min3photos     = activityResult.data?.getBooleanExtra("min3photos",false)!!
-            val schools     = activityResult.data?.getBooleanExtra("schools",false)!!
-            val shops     = activityResult.data?.getBooleanExtra("shops",false)!!
+    val launcherActivityResult =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == 0) {
+                filterState = activityResult.data?.getBooleanExtra("filterState", false)!!
+            }
+            if (activityResult.resultCode == 1) {
+                filterState = activityResult.data?.getBooleanExtra("filterState", true)!!
+                val type = activityResult.data?.getStringExtra("type")!!
+                val city = activityResult.data?.getStringExtra("city")!!
+                val minSurface = activityResult.data?.getIntExtra("minSurface", 0)!!
+                val maxSurface = activityResult.data?.getIntExtra("maxSurface", 0)!!
+                val minPrice = activityResult.data?.getIntExtra("minPrice", 0)!!
+                val maxPrice = activityResult.data?.getIntExtra("maxPrice", 0)!!
+                val onTheMarketLessALastWeek =
+                    activityResult.data?.getBooleanExtra("onTheMarketLessALastWeek", false)!!
+                val soldOn3LastMonth =
+                    activityResult.data?.getBooleanExtra("soldOn3LastMonth", false)!!
+                val min3photos = activityResult.data?.getBooleanExtra("min3photos", false)!!
+                val schools = activityResult.data?.getBooleanExtra("schools", false)!!
+                val shops = activityResult.data?.getBooleanExtra("shops", false)!!
 
-            val query ="SELECT * FROM RealEstateDatabase WHERE " +
-                    "('$type' ='' OR type = '$type') AND " +
-                    "('$city' ='' OR city = '$city') AND " +
-                    "($schools = false OR schoolsNear = true) AND " +
-                    "($shops = false OR shopsNear = true) AND " +
-                    "($min3photos = false OR count_photo >= 3) AND " +
-                    "($minSurface =0 AND $maxSurface =0 OR (area BETWEEN $minSurface AND $maxSurface)) AND " +
-                    "($minPrice =0 AND $maxPrice =0 OR (price BETWEEN $minPrice AND $maxPrice)) "
+                val c = Calendar.getInstance()
+                val fmt: DateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy")
 
-            Log.e("query",query)
+                val dateToday = DateTime(c.time)
+                val dateTodayFinal = dateToday.toString(fmt)
 
-            realEstateViewModel.getPropertyBySearch(SimpleSQLiteQuery(query)).observeForever{ list->
-                realEstatesFilter = list
+                val dateMinusThreeMonth = dateToday.minusMonths(3).toString(fmt)
+                val dateMinus1Week = dateToday.minusDays(7).toString(fmt)
+
+                val query = """SELECT * FROM RealEstateDatabase WHERE 
+                        ('$type' ='' OR type LIKE '%$type%' ) AND 
+                        ('$city' ='' OR city LIKE '%$city%' ) AND
+                        ($schools = false OR schoolsNear = $schools ) AND 
+                        ($shops = false OR shopsNear = $shops ) AND 
+                        ($min3photos = false OR count_photo >= 3 ) AND
+                        ($minSurface =0 AND $maxSurface = 0  OR  area BETWEEN $minSurface AND $maxSurface  ) AND 
+                        ($minPrice =0 AND $maxPrice = 0  OR  price BETWEEN $minPrice AND $maxPrice ) AND 
+                        ($onTheMarketLessALastWeek = false  OR  dateOfEntry BETWEEN '$dateMinus1Week' AND '$dateTodayFinal' ) AND 
+                        ($soldOn3LastMonth = false  OR  dateOfSale <> '00/00/0000' OR  dateOfSale BETWEEN '$dateMinusThreeMonth' AND '$dateTodayFinal' ) """
+
+
+
+
+
+                Log.e("query", query)
+
+                realEstateViewModel.getPropertyBySearch(SimpleSQLiteQuery(query))
+                    .observeForever { list ->
+                        realEstatesFilter = list
+                    }
             }
         }
-    }
 
 
 
@@ -95,96 +117,90 @@ fun ListScreen(
     if (windowSize == WindowSize.COMPACT) {
 
 
-                Scaffold(
-                    modifier = Modifier.padding(innerPadding),
-                    topBar = {
+        Scaffold(
+            modifier = Modifier.padding(innerPadding),
+            topBar = {
 
-                        CenterAlignedTopAppBar(
-                            title = {
-                                Text(text = "RealEstateManager")
-                            },
-                            navigationIcon = {
-                                IconButton(onClick = {
-                                    scope.launch { drawerState.open() }
-                                }) {
-                                    Icon(Icons.Filled.Menu, "")
-                                }
-                            },
-                            actions = {
-                                IconButton(onClick = {
-                                    launcherActivityResult.launch(
-                                        Intent(
-                                            context,
-                                            FilterActivity::class.java
-                                        )
-                                    )
-                                }) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_baseline_filter_list_24),
-                                        contentDescription = ""
-                                    )
-                                }
-                            },
-                        )
-
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "RealEstateManager")
                     },
-                    content = { it ->
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch { drawerState.open() }
+                        }) {
+                            Icon(Icons.Filled.Menu, "")
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            launcherActivityResult.launch(
+                                Intent(
+                                    context,
+                                    FilterActivity::class.java
+                                )
+                            )
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_filter_list_24),
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                )
 
-                                Log.e("realEstatesResponse", "Success")
+            },
+            content = { it ->
 
-                        val isRefreshing by realEstateViewModel.isRefreshing.collectAsState()
+                Log.e("realEstatesResponse", "Success")
 
-
-
-                                SwipeRefresh(
-                                    modifier = Modifier.padding(it),
-                                    state = rememberSwipeRefreshState(isRefreshing),
-                                    onRefresh = {
-                                                realEstateViewModel.refreshRealEstates()
-                                    },
-                                ) {
-
-
-
-                                        LazyColumn {
-                                            if(!filterState) {
-
-                                                        items(realEstates) { item ->
-                                                            RowList(
-                                                                item,
-                                                                realEstateViewModel,
-                                                                navControllerDrawer,
-                                                                windowSize,
-                                                                navControllerTwoPane
-                                                            )
-                                                        }
-
-
-                                            }else{
+                val isRefreshing by realEstateViewModel.isRefreshing.collectAsState()
 
 
 
+                SwipeRefresh(
+                    modifier = Modifier.padding(it),
+                    state = rememberSwipeRefreshState(isRefreshing),
+                    onRefresh = {
+                        realEstateViewModel.refreshRealEstates()
+                    },
+                ) {
 
 
+                    LazyColumn {
+                        if (!filterState) {
+
+                            items(realEstates) { item ->
+                                RowList(
+                                    item,
+                                    realEstateViewModel,
+                                    navControllerDrawer,
+                                    windowSize,
+                                    navControllerTwoPane
+                                )
+                            }
 
 
-                                                        items(realEstatesFilter) { item ->
-                                                            RowList(
-                                                                item,
-                                                                realEstateViewModel,
-                                                                navControllerDrawer,
-                                                                windowSize,
-                                                                navControllerTwoPane
-                                                            )
-                                                        }
+                        } else {
 
-                                            }
 
-                                        }
-                                }
+                            items(realEstatesFilter) { item ->
+                                RowList(
+                                    item,
+                                    realEstateViewModel,
+                                    navControllerDrawer,
+                                    windowSize,
+                                    navControllerTwoPane
+                                )
+                            }
+
+                        }
 
                     }
-                )
+                }
+
+            }
+        )
     }
 }
 
