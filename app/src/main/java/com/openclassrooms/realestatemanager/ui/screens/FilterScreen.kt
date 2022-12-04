26eb849f42ea2078f9695336084478df0d56f0e0
azubal <valentinfamery087@gmail.com>
2022-12-04 17:17:48 +0100

    fix(): change filter screen , size , alignement

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/FilterScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/screens/FilterScreen.kt
@@ -3,13 +3,17 @@ package com.openclassrooms.realestatemanager.ui.screens
 import android.app.Activity
 import android.content.Intent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -44,7 +48,9 @@ fun FilterScreen() {
     else
         Icons.Filled.KeyboardArrowDown
 
-ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+ConstraintLayout(modifier = Modifier
+    .fillMaxSize()
+    .verticalScroll(rememberScrollState())) {
 
     val (buttonClose,buttonReset,column,buttonFilter) = createRefs()
 
@@ -65,14 +71,17 @@ ConstraintLayout(modifier = Modifier.fillMaxSize()) {
         Text("Reset")
     }
 
-    Column(modifier = Modifier.constrainAs(column) {
-        top.linkTo(parent.top, margin = 50.dp)
-        start.linkTo(parent.start)
-        end.linkTo(parent.end)
-    }) {
+    Column(modifier = Modifier
+        .constrainAs(column) {
+            top.linkTo(buttonReset.bottom, margin = 25.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+        .fillMaxWidth(0.8f)
+        .wrapContentHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
 
         TextField(
-            value = entryType.toString(),
+            value = entryType,
             onValueChange = { entryType = it },
             label = { Text("Type") },
             trailingIcon = {
@@ -97,57 +106,95 @@ ConstraintLayout(modifier = Modifier.fillMaxSize()) {
                 }
             }
 
+        Spacer(modifier = Modifier.size(10.dp))
+
         TextField(
-            value = entryCity.toString(),
+            value = entryCity,
             onValueChange = { entryCity = it },
-            label = { Text("City") },
-            singleLine = true
+            label = { Text("City") }
         )
 
-        Row() {
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row(horizontalArrangement = Arrangement.Start) {
             TextField(
                 value = entryMinSurface,
                 onValueChange = { entryMinSurface = it },
                 label = { Text("MinSurface") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(0.4f)
+                modifier = Modifier.fillMaxWidth(0.5f)
             )
             TextField(
                 value = entryMaxSurface,
                 onValueChange = { entryMaxSurface = it },
                 label = { Text("MaxSurface") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(0.4f)
+                modifier = Modifier.fillMaxWidth(0.5f)
             )
         }
 
-        Row() {
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row(horizontalArrangement = Arrangement.Start) {
             TextField(
                 value = entryMinPrice,
                 onValueChange = { entryMinPrice = it },
                 label = { Text("MinPrice") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(0.4f)
+                modifier = Modifier.fillMaxWidth(0.5f)
             )
             TextField(
                 value = entryMaxPrice,
                 onValueChange = { entryMaxPrice = it },
                 label = { Text("MaxPrice") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(0.4f)
+                modifier = Modifier.fillMaxWidth(0.5f)
             )
         }
 
-        Text(text = "onTheMarketLessALastWeek")
-        Checkbox(checked = onTheMarketLessALastWeek, onCheckedChange ={onTheMarketLessALastWeek = it})
-        Text(text = "soldOn3LastMonth")
-        Checkbox(checked = soldOn3LastMonth, onCheckedChange ={soldOn3LastMonth = it})
-        Text(text = "min3photos")
-        Checkbox(checked = min3photos, onCheckedChange ={min3photos = it})
-        Text(text = "schools")
-        Checkbox(checked = schools, onCheckedChange ={schools = it})
-        Text(text = "shops")
-        Checkbox(checked = shops, onCheckedChange ={shops = it})
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(text = "The property was put on the market less than a week ago")
+            Checkbox(
+                checked = onTheMarketLessALastWeek,
+                onCheckedChange = { onTheMarketLessALastWeek = it })
+            Text(text = "The property has been sold in the last three months")
+            Checkbox(checked = soldOn3LastMonth, onCheckedChange = { soldOn3LastMonth = it })
+            Text(text = "The property has at least three photos")
+            Checkbox(checked = min3photos, onCheckedChange = { min3photos = it })
+            Text(text = "The property has schools nearby")
+            Checkbox(checked = schools, onCheckedChange = { schools = it })
+            Text(text = "The property has shops nearby")
+            Checkbox(checked = shops, onCheckedChange = { shops = it })
+        }
+
+
+        Spacer(modifier = Modifier.size(10.dp))
+        Button(
+            onClick = {
+
+                filterState = true
+
+                val intent = Intent()
+
+                intent.putExtra("type",entryType)
+                intent.putExtra("city",entryCity)
+                intent.putExtra("minSurface", entryMinSurface.toInt())
+                intent.putExtra("maxSurface",entryMaxSurface.toInt())
+                intent.putExtra("minPrice",entryMinPrice.toInt())
+                intent.putExtra("maxPrice",entryMaxPrice.toInt())
+                intent.putExtra("onTheMarketLessALastWeek",onTheMarketLessALastWeek)
+                intent.putExtra("soldOn3LastMonth",soldOn3LastMonth)
+                intent.putExtra("min3photos",min3photos)
+                intent.putExtra("schools",schools)
+                intent.putExtra("shops",shops)
+
+                intent.putExtra("filterState",filterState)
+
+                activity.setResult(1,intent)
+                activity.finish()
+            },
+        ) {
+            Text("Filter")
+        }
+        Spacer(modifier = Modifier.size(50.dp))
     }
 
 
@@ -168,37 +215,7 @@ ConstraintLayout(modifier = Modifier.fillMaxSize()) {
         )
     }
 
-    Button(
-        onClick = {
-
-            filterState = true
-
-        val intent = Intent()
-
-            intent.putExtra("type",entryType)
-            intent.putExtra("city",entryCity)
-            intent.putExtra("minSurface", entryMinSurface.toInt())
-            intent.putExtra("maxSurface",entryMaxSurface.toInt())
-            intent.putExtra("minPrice",entryMinPrice.toInt())
-            intent.putExtra("maxPrice",entryMaxPrice.toInt())
-            intent.putExtra("onTheMarketLessALastWeek",onTheMarketLessALastWeek)
-            intent.putExtra("soldOn3LastMonth",soldOn3LastMonth)
-            intent.putExtra("min3photos",min3photos)
-            intent.putExtra("schools",schools)
-            intent.putExtra("shops",shops)
-
-            intent.putExtra("filterState",filterState)
-
-        activity.setResult(1,intent)
-            activity.finish()
-    },
-        modifier = Modifier.constrainAs(buttonFilter){
-            bottom.linkTo(parent.bottom, margin = 10.dp)
-        }
-
-    ) {
-        Text("Filter")
-    }
+    
 
 }
 }
